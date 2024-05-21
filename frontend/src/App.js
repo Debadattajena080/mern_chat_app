23ac905ddf0e76f3a16e7933fecd1c53d880d42e
DEBADATTAJENA <debadattajena25@gmail.com>
2024-05-21 07:26:09 +0530

    frontend structure added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signUp/SignUp";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="p-4 h-screen flex items-center justify-center">
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      <Home />
     </div>
   );
 }
---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div>
+      <h3>
+        This is a message,Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Id pariatur aspernatur omnis ad voluptatum enim earum, consequatur
+        rerum ipsa iste. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Ut illo voluptas ducimus impedit ratione, ea, velit itaque
+        inventore magnam hic accusamus. Architecto provident, ut accusantium,
+        nostrum quis fuga nisi laborum omnis illum aliquid numquam quae ad?
+        Accusamus iure, vel modi iste architecto, non nulla adipisci
+        praesentium, dolorem facere temporibus tempora.
+      </h3>
+    </div>
+  );
+};
+
+export default Message;
